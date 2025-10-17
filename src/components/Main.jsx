2e41a7caa274a3c856c2e93d7a1d99a4b1eb701d
huydunk <89574667+huydunk@users.noreply.github.com>
2025-10-17 11:38:59 +0700

    feature: only display section only when there's > 0 ingredients

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react"
 
 export default function Main() {
-    const [ingredients, setIngredients] = useState(["Chicken", "Oregano", "Tomatoes"])
+    const [ingredients, setIngredients] = useState([])
 
     const ingredientsListItems = ingredients.map(ingredient => (
         <li key={ingredient}>{ingredient}</li>
@@ -30,17 +30,21 @@ export default function Main() {
                 <button>Add ingredient</button>
 
             </form>
-            <section>
-                <h2>Ingredients on hand:</h2>
-                <ul className="ingredients-list" aria-live="polite">{ingredientsListItems}</ul>
-                <div className="get-recipe-container">
-                    <div>
-                        <h3>Ready for a recipe?</h3>
-                        <p>Generate a recipe from your list of ingredients.</p>
-                    </div>
-                    <button>Get a recipe</button>
-                </div>
-            </section>
+            {ingredients.length > 0 && (
+                    <section>
+                        <h2>Ingredients on hand:</h2>
+                        <ul className="ingredients-list" aria-live="polite">{ingredientsListItems}</ul>
+                        <div className="get-recipe-container">
+                            <div>
+                                <h3>Ready for a recipe?</h3>
+                                <p>Generate a recipe from your list of ingredients.</p>
+                            </div>
+                            <button>Get a recipe</button>
+                        </div>
+                    </section>
+                )
+            }
+
         </main>
     )
 
